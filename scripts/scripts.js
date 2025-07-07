883970c3ef6c6a314dcdb7518f2e957996cdf6b5
chuamj <meng.joo.chua@gmail.com>
2025-07-07 13:35:02 +0800

    Delete scripts directory

--- scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-    function validateForm() {
-      const name = document.getElementById("query").value;
-      document.getElementById("message").innerText = name
-        ? alert("Search Engine not ready yet!")
-        : alert("Name cannot be empty.");
-    }
